3266f172277d9f128bd98ac87a69196336a2535b
hospice tanguy <htcvhs971@gmail.com>
2022-03-25 19:17:05 -0400

    Creation dossiers pages et components & pages: Home.js + About.js

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>A Propos</h1>
+			<br />
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum possimus ut explicabo a aliquam quod temporibus deserunt obcaecati minima
+				praesentium perferendis, ipsum consectetur repudiandae repellendus voluptatibus illum recusandae dicta cum odit harum nihil accusamus
+				excepturi? Est mollitia nam ea quod, commodi debitis nostrum minima, rem, tenetur ducimus eligendi velit possimus perspiciatis a quia
+				explicabo repellendus quibusdam corporis? Possimus molestiae ipsum temporibus quae minus est corrupti? Quia necessitatibus vitae repudiandae
+				facilis esse, nihil corporis at repellendus saepe in sapiente temporibus illum animi distinctio. Pariatur blanditiis nemo hic obcaecati
+				quisquam a beatae accusamus tempora accusantium, molestias doloribus facere. Aliquam tenetur unde consequatur!
+			</p>
+			<br />
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias officiis laudantium aspernatur impedit quo, dignissimos sunt aperiam debitis
+				porro earum laborum laboriosam eius illo, iste magnam doloremque consectetur cumque. Aspernatur?
+			</p>
+		</div>
+	);
+};
+
+export default About;
